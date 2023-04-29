42b1609d49761f3cdc2b2467151174661c6391c0
dgwyther <david.gwyther@gmail.com>
2023-04-29 13:40:07 +1000

    tweaks to fig exp2

--- notebooks/plot_figure_exp2_NCI.m
+++ notebooks/plot_figure_exp2_NCI.m
@@ -86,12 +86,13 @@ p2s=plot(NoFlow.ot,filter1('lp',NoFlow.my,'fs',Fs,'fc',fc,'order',5),'linewidth'
 p3s=plot(Norm.ot,filter1('lp',Norm.my,'fs',Fs,'fc',fc,'order',5),'linewidth',2,'color',C(2,:));
 p4s=plot(Norm_TO.ot,filter1('lp',Norm_TO.my,'fs',Fs,'fc',fc,'order',5),'linewidth',2,'color',C(3,:));
 p5s=plot(Norm_SO.ot,filter1('lp',Norm_SO.my,'fs',Fs,'fc',fc,'order',5),'linewidth',2,'color',C(4,:));
-legend([p2s,p3s,p4s,p5s],{'no flow','with SGFW','temp only','salt only'},'location','northeast'),legend('boxoff'),grid on,set(gca,'layer','top'),ylabel('melt rate (m/yr)')
-ntitle(' a','location','northwest','fontweight','bold','fontsize',12)
+legend([p2s,p3s,p4s,p5s],{'no flow','with SGFW','temp only','salt only'},'location','northeast'),
+legend('boxoff'),grid on,set(gca,'layer','top'),ylabel('melt rate (m/yr)',FontWeight='bold')
+ntitle(' a','location','northwest','fontweight','bold','fontsize',10)
 axis([20 21 7 9.5])
 xticks([20:1/12:21])
 xticklabels({'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec',''})
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 set(gca,'pos',[0.1200    0.7882    0.8300    0.1818])
 %
 subaxis(4,3,4)
@@ -100,16 +101,16 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 %caxis([-10 10])
 cmocean('solar')
-ntitle(' b','location','northwest','fontweight','bold','fontsize',12)
-title('Melt','fontsize',12,'fontweight','bold')
-ylabel('with SGFW','fontsize',12,'fontweight','bold')
+ntitle(' b','location','northwest','fontweight','bold','fontsize',10)
+title('Melt','fontsize',10,'fontweight','bold')
+ylabel('with SGFW','fontsize',10,'fontweight','bold')
 h_cb1=colorbar('southoutside');
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 grid on
 set(gca,'layer','top')
 set(gca,'xticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,7)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,(nanmean(Norm_TO.melt,3)-nanmean(NoFlow.melt,3))./(nanmean(NoFlow.melt,3)).*mask_totten_nan*100);
@@ -117,15 +118,15 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([-10 10])
 cmocean('balance','pivot',0)
-ntitle(' e','location','northwest','fontweight','bold','fontsize',12)
-ylabel('Temp-only (% diff to Ctrl.)','fontsize',12,'fontweight','bold')
+ntitle(' e','location','northwest','fontweight','bold','fontsize',10)
+ylabel('Temperature only (% diff)','fontsize',10,'fontweight','bold')
 ntitle([num2str(round((nanmean(Norm_TO.my./NoFlow.my)-1)*100,1)),'%'],'location','northeast')
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 set(gca,'xticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,10)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,(nanmean(Norm_SO.melt,3)-nanmean(NoFlow.melt,3))./(nanmean(NoFlow.melt,3)).*mask_totten_nan*100);
@@ -133,14 +134,14 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([-10 10])
 cmocean('balance','pivot',0)
-ntitle(' h','location','northwest','fontweight','bold','fontsize',12)
-ylabel('Salt only (% diff. to Ctrl.)','fontsize',12,'fontweight','bold')
+ntitle(' h','location','northwest','fontweight','bold','fontsize',10)
+ylabel('Salt only (% diff)','fontsize',10,'fontweight','bold')
 ntitle([num2str(round((nanmean(Norm_SO.my./NoFlow.my)-1)*100,1)),'%'],'location','northeast'),
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
-set(gca,'fontsize',11)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,5)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,nanmean(Norm.td,3).*mask_totten_nan);
@@ -148,17 +149,17 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([0 2.5])
 cmocean('thermal')
-ntitle(' c','location','northwest','fontweight','bold','fontsize',12)
-title('Thermal driving','fontweight','bold','fontsize',12)
+ntitle(' c','location','northwest','fontweight','bold','fontsize',10)
+title('Thermal driving','fontweight','bold','fontsize',10)
 %ylabel('Thermal driving','fontsize',8,'fontweight','bold')
 h_cb2=colorbar('southoutside');
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 set(gca,'xticklabels',[])
 set(gca,'yticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,8)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,(nanmean(Norm_TO.td,3)-nanmean(NoFlow.td,3))./(nanmean(NoFlow.td,3)).*mask_totten_nan*100);
@@ -166,14 +167,14 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([-10 10])
 cmocean('balance','pivot',0)
-ntitle(' f','location','northwest','fontweight','bold','fontsize',12)
+ntitle(' f','location','northwest','fontweight','bold','fontsize',10)
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 set(gca,'xticklabels',[])
 set(gca,'yticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,11)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,(nanmean(Norm_SO.td,3)-nanmean(NoFlow.td,3))./(nanmean(NoFlow.td,3)).*mask_totten_nan*100);
@@ -181,32 +182,32 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([-10 10])
 cmocean('balance','pivot',0)
-ntitle(' i','location','northwest','fontweight','bold','fontsize',12)
+ntitle(' i','location','northwest','fontweight','bold','fontsize',10)
 h_cb4=colorbar('southoutside');
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 set(gca,'yticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,6)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,nanmean(Norm.ustar,3).*mask_totten_nan);
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','linewidth',2) %grounding line
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([0 0.35])
-cmocean('-haline')
-ntitle(' d','location','northwest','fontweight','bold','fontsize',12)
-title('Friction velocity','fontweight','bold','fontsize',12)
+cmocean('haline')
+ntitle(' d','location','northwest','fontweight','bold','fontsize',10)
+title('Friction velocity','fontweight','bold','fontsize',10)
 %ylabel('Friction velocity','fontsize',8,'fontweight','bold')
 h_cb3=colorbar('southoutside');
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 set(gca,'xticklabels',[])
 set(gca,'yticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,9)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,(nanmean(Norm_TO.ustar,3)-nanmean(NoFlow.ustar,3))./nanmean(NoFlow.ustar,3)*100.*mask_totten_nan);
@@ -214,14 +215,14 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([-10 10])
 cmocean('balance','pivot',0)
-ntitle(' g','location','northwest','fontweight','bold','fontsize',12)
+ntitle(' g','location','northwest','fontweight','bold','fontsize',10)
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 set(gca,'xticklabels',[])
 set(gca,'yticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 %
 subaxis(4,3,12)
 flat_pcolor(NoFlow_coords.x,NoFlow_coords.y,(nanmean(Norm_SO.ustar,3)-nanmean(NoFlow.ustar,3))./nanmean(NoFlow.ustar,3)*100.*mask_totten_nan);
@@ -229,22 +230,22 @@ hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.h,[20:1:21],'k-','li
 hold on,contour(NoFlow_coords.x,NoFlow_coords.y,NoFlow_geom.zice,[-1:1:0],'k:','linewidth',2) %icefront line
 caxis([-10 10])
 cmocean('balance','pivot',0)
-ntitle(' j','location','northwest','fontweight','bold','fontsize',12)
+ntitle(' j','location','northwest','fontweight','bold','fontsize',10)
 grid on
 set(gca,'layer','top')
 axis equal
-axis([2.2e6 2.35e6 -1.17e6 -9.8537e5]/1000)
+axis([2.21e6 2.35e6 -1.17e6 -9.8537e5]/1000)
 set(gca,'yticklabels',[])
-set(gca,'fontsize',11)
+set(gca,'fontsize',9)
 
 set(h_cb2,'pos',[0.28    0.03    0.2    0.0127])
 set(h_cb1,'pos',[0.03    0.03    0.2    0.0127])
 set(h_cb3,'pos',[0.53    0.03    0.2    0.0127])
 set(h_cb4,'pos',[0.78    0.03    0.2    0.0127])
-title(h_cb1,'melt rate (m/yr)','fontsize',12)
-title(h_cb2,'T* (\circ C)','fontsize',12)
-title(h_cb3,'u* (m/s)','fontsize',12)
-title(h_cb4,'%\Delta to ctrl. (no flow)','fontsize',12)
+title(h_cb1,'melt rate (m/yr)','fontsize',10)
+title(h_cb2,'T* (\circ C)','fontsize',10)
+title(h_cb3,'u* (m/s)','fontsize',10)
+title(h_cb4,'%\Delta to No Flow','fontsize',10)
 
 set(gcf,'pos',[300    55   584   850])
 
